bae992c35dc486cd0c4a2c8bd8a2847bc5bae0d9
MAXON <61685418+Maxonpoll@users.noreply.github.com>
2023-12-20 19:08:56 -0800

    Add files via upload

---
+++ Donate.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+// Handle the donation processing here based on the selected payment method
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $amount = $_POST["amount"];
+    $paymentMethod = $_POST["paymentMethod"];
+
+    switch ($paymentMethod) {
+        case "paytm":
+            // Handle Paytm payment processing
+            break;
+
+        case "phonepe":
+            // Handle PhonePe payment processing
+            break;
+
+        case "googlepay":
+            // Handle Google Pay payment processing
+            break;
+
+        case "paypal":
+            // Handle PayPal payment processing
+            break;
+
+        default:
+            // Handle other cases or show an error
+            break;
+    }
+}
+
+// Add more processing logic, database updates, or redirects as needed
+
+?>
---
+++ donate.js
@@ -0,0 +1,4 @@
+﻿// You can add client-side validation and other functionalities here
+$(document).ready(function() {
+    // Additional client-side scripting if needed
+});
